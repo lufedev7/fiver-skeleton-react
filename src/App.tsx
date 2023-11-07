b84e6142f88d6db5c87155f00af66b53044b3f1f
lufedev <luisferan738@gmail.com>
2023-11-07 11:55:44 -0500

    error5

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-imp ort './App.css'3
-3
+import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
